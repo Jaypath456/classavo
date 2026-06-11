dba44b507ae4fb75c1f77f0b8ef4ad6ceebddd2c
Jay Pathare <jaypathare123@gmail.com>
2026-06-11 12:54:05 -0400

    managing environment variables in Django for database

--- backend/lms_project/settings.py
+++ backend/lms_project/settings.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = 'django-insecure-lms-classavo-internship-key-2026'
 
